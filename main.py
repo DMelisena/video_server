be2c7c86a2c527d81143fabba4a7e3e62c9c65e1
DMelisena <demonlordx777@gmail.com>
2025-08-21 21:46:05 +0800

    feat: server initiation

---
+++ main.py
@@ -0,0 +1,19 @@
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/upload', methods=['POST'])
+def upload_video():
+    if 'video' not in request.files:
+        return 'No video file found', 400
+    
+    video = request.files['video']
+    
+    # You can optionally save the video file
+    # video.save('uploaded_video.mp4')
+
+    return 'its a video'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8081, debug=True)
